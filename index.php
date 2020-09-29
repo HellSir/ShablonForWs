6faad30271d95d2065af54a62af6de4b8e753496
HellSir <72050902+HellSir@users.noreply.github.com>
2020-09-30 01:17:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8" />
+    <title>Шаблон</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+<header class="menu-bar">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-2">
+                <img src="image/1.png" alt="My Logo" class="logo">
+            </div>
+            <div class="col-xl-8  ml-auto align-self-center">
+                <nav>
+                    <ul class="menu d-flex justify-contend-end">
+                        <li class="menu__item"><a href="#">Home</a></li>
+                        <li class="menu__item"><a href="#">About</a></li>
+                        <li class="menu__item"><a href="#">Servicing</a></li>
+                        <li class="menu__item"><a href="#">Portfolio</a></li>
+                        <li class="menu__item"><a href="#">Blog</a></li>
+                        <li class="menu__item"><a href="#">Contact</a></li>
+                        <li class="menu__item"><a href="#">Search</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<header class="header">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <h1 class="header__title">Web project for WS</h1>
+                <p class="header__text">My template for WS</p>
+                <button class="button button_empty">Our services</button>
+                <button class="button button_full">Hire is now</button>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="about">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-8">
+                <h2 class="about__title">About us</h2>
+                <p  class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci incidunt iusto molestias quod? Animi explicabo fuga laboriosam magnam tempora temporibus unde!</p>
+            </div >
+        </div>
+        <div class="row justify-content-around">
+            <div class="col-xl-3"><img src="image/internet.png" alt="">
+                <h3 class="about__h3">Awesom icon</h3>
+                <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio dolorem omnis sed sit temporibus veniam?</p>
+            </div>
+            <div class="col-xl-3"><img src="image/transfer.png" alt="">
+                <h3 class="about__h3">One Page</h3>
+                <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic molestiae optio provident quia rem. Quod!</p>
+            </div>
+            <div class="col-xl-3"><img src="image/head.png" alt="">
+                <h3 class="about__h3">Fully Responsive</h3>
+                <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi error magni maxime odio, repudiandae sapiente?</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="services">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-8">
+                <h2 class="about__title">Our services</h2>
+                <p  class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci incidunt iusto molestias quod? Animi explicabo fuga laboriosam magnam tempora temporibus unde!</p>
+            </div >
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-xl-4">
+                <div class="our_card">
+                    <i class="fas fa-mobile-alt"></i>
+                    <h2 class="about__title">Our services</h2>
+                    <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci incidunt iusto molestias quod? Animi explicabo fuga laboriosam magnam tempora temporibus unde!</p>
+                    <button class="button_services">Read more</button>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="our_card">
+                    <i class="fas fa-mobile-alt"></i>
+                    <h2 class="about__title">Our services</h2>
+                    <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci incidunt iusto molestias quod? Animi explicabo fuga laboriosam magnam tempora temporibus unde!</p>
+                    <button class="button_services">Read more</button>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="our_card">
+                    <i class="fas fa-mobile-alt"></i>
+                    <h2 class="about__title">Our services</h2>
+                    <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci incidunt iusto molestias quod? Animi explicabo fuga laboriosam magnam tempora temporibus unde!</p>
+                    <button class="button_services">Read more</button>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="our_card">
+                    <i class="fas fa-mobile-alt"></i>
+                    <h2 class="about__title">Our services</h2>
+                    <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci incidunt iusto molestias quod? Animi explicabo fuga laboriosam magnam tempora temporibus unde!</p>
+                    <button class="button_services">Read more</button>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="our_card">
+                    <i class="fas fa-mobile-alt"></i>
+                    <h2 class="about__title">Our services</h2>
+                    <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci incidunt iusto molestias quod? Animi explicabo fuga laboriosam magnam tempora temporibus unde!</p>
+                    <button class="button_services">Read more</button>
+                </div>
+            </div>
+            <div class="col-xl-4">
+                <div class="our_card">
+                    <i class="fas fa-mobile-alt"></i>
+                    <h2 class="about__title">Our services</h2>
+                    <p class="about__text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci incidunt iusto molestias quod? Animi explicabo fuga laboriosam magnam tempora temporibus unde!</p>
+                    <button class="button_services">Read more</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
